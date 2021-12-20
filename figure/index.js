de54a580dd3e6fcf35681a18ac0fb2bf6477b4df
sjk <lueky4300@naver.com>
2021-12-20 16:55:55 +0800

    printExecutionSequence함수 리팩토링 순서, 결과도 함께 출력되도록 수정

--- figure/index.js
+++ figure/index.js
@@ -7,21 +7,18 @@ const callFunc = [];
 // 원의 넓이 = 반지름 * (원주 / 2) or 반지름제곱 * PI
 function getCircle(radius) {
     if(!isTypeNumCheck(radius)) errorMessage(isTypeNumberErrorMessage);
-    callFunc.push('circle');
     return radius * radius * PI;
 }
 
 // 사각형의 넓이 = 가로 x 세로
 function getRect(width, vertical) {
     if(!isTypeNumCheck(width, vertical)) errorMessage(isTypeNumberErrorMessage);
-    callFunc.push('rect');
     return width * vertical;
 }
 
 // 사다리꼴의 넓이 = (가로 + 세로) / 2 * h
 function getTrapezoid(width, vertical, height) {
     if(!isTypeNumCheck(width, vertical, height)) errorMessage(isTypeNumberErrorMessage);
-    callFunc.push('trapezoid');
     return (width + vertical) / 2 * height;
 }
 
@@ -34,7 +31,6 @@ function getCylinder(radius, height) {
 
 function getCylinderSide(radius, height) {
     if(!isTypeNumCheck(radius, height)) errorMessage(isTypeNumberErrorMessage);
-    callFunc.push('cylinder');
     return 2 * PI * radius * height;
 }
 
@@ -56,6 +52,7 @@ function errorMessage(message) {
 }
 
 function getArea(figure, ...args) {
+    let result;
     if(figure === 'circle') {
         if(args.length === 2) {
             const n = args[1];
@@ -63,33 +60,42 @@ function getArea(figure, ...args) {
             for(let i = args[0]; i <= n; i++) {
                 sum += getCircle(i)
             }
-            return sum;
+            result = sum;
         } else {
             if(!isParamNumCheck(args, 1)) errorMessage(paramErrorMessage);
-            return getCircle(args[0]);
+            result = getCircle(args[0]);
         }
     }
     else if(figure === 'rect') {
         if(!isParamNumCheck(args, 2)) errorMessage(paramErrorMessage);
-        return getRect(args[0], args[1]);
+        result = getRect(args[0], args[1]);
     }
     else if(figure === 'trapezoid') {
         if(!isParamNumCheck(args, 3)) errorMessage(paramErrorMessage);
-        return getTrapezoid(args[0], args[1], args[2]);
+        result = getTrapezoid(args[0], args[1], args[2]);
     }
     else if(figure === 'cylinder') {
         if(!isParamNumCheck(args, 2)) errorMessage(paramErrorMessage);
-        return getCylinder(args[0], args[1]);
+        result = getCylinder(args[0], args[1]);
     }
+    const obj = {};
+    obj[figure] = result;
+    callFunc.push(obj);
+    return result;
 }
 
 function printExecutionSequence() {
-    return `계산수행순서 : ${callFunc.join(", ")}`
+    let result = "";
+    callFunc.forEach((element, index) => {
+        const key = Object.keys(element);
+        const value = Object.values(element);
+        if(index === callFunc.length - 1) result += `${key}: ${value}`
+        else result += `${key}: ${value}, `
+    });
+    return result;
 }
 
-getCircle(3) 
-getCircle(4) 
-getArea('circle',2) 
-getArea('rect',2,3) 
+getArea('circle', 2)
+getArea('rect', 2, 3)
 
 console.log(printExecutionSequence());
