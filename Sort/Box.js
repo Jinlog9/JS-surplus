35bc566ba99cd308db8f5ff30096c964d5d8a95a
Muffin <jinlog9@gmail.com>
2022-03-30 12:38:17 +0900

    feat, refactor: 초기화하는 부분 우선은 Box Class 함수에서 처리

--- Sort/Box.js
+++ Sort/Box.js
@@ -1,16 +1,44 @@
+import { randomData, checkTime } from "./util.js";
+import { colors } from "./constants.js";
 export default class Box {
-    el;
-    size;
-    static boxes = new Set();
-    constructor(size, bgColor) {
-        this.el = document.createElement("div");
-        this.size = size;   
-        this.el.style.height = `${10 * this.size}px`;
-        this.el.style.backgroundColor = `${bgColor}`;
-        this.el.classList.add("box");
+  el;
+  size;
+  static boxes = [];
+  constructor(id, size, bgColor) {
+    this.el = document.createElement("div");
+    this.el.id = id;
+    this.size = size;
+    this.el.style.height = `${10 * this.size}px`;
+    this.el.style.backgroundColor = `${bgColor}`;
+    this.el.classList.add("box");
+  }
+
+  static valueInit() {
+    const section = document.querySelector(".sort-wrapper");
+    section.innerHTML = "";
+    this.boxes = [];
+    for (let i = 0; i < randomData.length; i++) {
+      const box = new Box(i, randomData[i], colors[i]);
+      this.boxes.push(box);
     }
 
-    render() {
-        // 두 값을 바꿔서 다시 그려주는 함수
+    for (let item of Box.boxes) {
+      section.appendChild(item.el);
     }
+  }
+
+  static swapBox(index1, index2) {
+    let temp = this.boxes[index1];
+    this.boxes[index1] = this.boxes[index2];
+    this.boxes[index2] = temp;
+
+    this.render();
+  }
+
+  static render() {
+    const section = document.querySelector(".sort-wrapper");
+    for (let item of Box.boxes) {
+      section.appendChild(item.el);
+    }
+  }
 }
